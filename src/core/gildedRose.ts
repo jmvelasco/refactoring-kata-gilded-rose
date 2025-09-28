af540c43547f488d5ba59e347356dfca4327aacd
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-09-28 23:06:49 +0200

    initial refactor, before watch solution

--- src/core/gildedRose.ts
+++ src/core/gildedRose.ts
@@ -13,57 +13,77 @@ export class Item {
 export class GildedRose {
 	items: Array<Item>;
 
-	constructor(items = [] as Array<Item>) {
+	constructor(items = []) {
 		this.items = items;
 	}
 
 	updateQuality() {
 		for (let i = 0; i < this.items.length; i++) {
+			if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
+				continue;
+			}
 			if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-				if (this.items[i].quality > 0) {
-					if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-						this.items[i].quality = this.items[i].quality - 1
-					}
+				if (this.canDecreaseItemQuality(i)) {
+					this.decreaseItemQuality(i);
 				}
 			} else {
-				if (this.items[i].quality < 50) {
-					this.items[i].quality = this.items[i].quality + 1
-					if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-						if (this.items[i].sellIn < 11) {
-							if (this.items[i].quality < 50) {
-								this.items[i].quality = this.items[i].quality + 1
-							}
-						}
-						if (this.items[i].sellIn < 6) {
-							if (this.items[i].quality < 50) {
-								this.items[i].quality = this.items[i].quality + 1
-							}
-						}
-					}
+				if (this.canIncreaseQuality(i)) {
+					this.increaseItemQuality(i);
+					this.itemInTheBackstage(i);
 				}
 			}
-			if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-				this.items[i].sellIn = this.items[i].sellIn - 1;
-			}
+			this.decreaseItemSellIndays(i);
 			if (this.items[i].sellIn < 0) {
 				if (this.items[i].name != 'Aged Brie') {
 					if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-						if (this.items[i].quality > 0) {
-							if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-								this.items[i].quality = this.items[i].quality - 1
-							}
+						if (this.canDecreaseItemQuality(i)) {
+							this.decreaseItemQuality(i);
 						}
 					} else {
-						this.items[i].quality = this.items[i].quality - this.items[i].quality
+						this.items[i].quality = this.items[i].quality - this.items[i].quality;
 					}
 				} else {
-					if (this.items[i].quality < 50) {
-						this.items[i].quality = this.items[i].quality + 1
+					if (this.canIncreaseQuality(i)) {
+						this.increaseItemQuality(i);
 					}
 				}
 			}
 		}
-
 		return this.items;
+	}
+
+	private canDecreaseItemQuality(i: number) {
+		return this.items[i].quality > 0;
+	}
+
+	private itemInTheBackstage(i: number) {
+		if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+			if (this.items[i].sellIn < 11) {
+				if (this.canIncreaseQuality(i)) {
+					this.increaseItemQuality(i);
+				}
+			}
+			if (this.items[i].sellIn < 6) {
+				if (this.canIncreaseQuality(i)) {
+					this.increaseItemQuality(i);
+				}
+			}
+		}
+	}
+
+	private decreaseItemSellIndays(i: number) {
+		this.items[i].sellIn = this.items[i].sellIn - 1;
+	}
+
+	private canIncreaseQuality(i: number) {
+		return this.items[i].quality < 50;
+	}
+
+	private decreaseItemQuality(i: number) {
+		this.items[i].quality = this.items[i].quality - 1;
+	}
+
+	private increaseItemQuality(i: number) {
+		this.items[i].quality = this.items[i].quality + 1;
 	}
 }
--- src/tests/gildedRose.test.ts
+++ src/tests/gildedRose.test.ts
@@ -2,10 +2,31 @@ import { Item, GildedRose } from '../core/gildedRose';
 
 describe('The Gilded Rose', () => {
 	it('updates quality for a new item', () => {
-		const gildedRose = new GildedRose([new Item('new item', 0, 0)]);
+		const gildedRose = new GildedRose(
+			generationCombinationOfItems(
+				['new item', 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'],
+				range(-1, 11),
+				range(0, 51)
+			)
+		);
 
 		const items = gildedRose.updateQuality();
 
-		expect(items[0]).toEqual({"name": "new item", "quality": 0, "sellIn": -1});
+		expect(items).toMatchSnapshot();
 	});
 });
+
+function generationCombinationOfItems(names: string[], sellInDays: number[], qualities: number[]) {
+	return names.flatMap((name) => {
+		return sellInDays.flatMap((sellInDay) => {
+			return qualities.flatMap((qualities) => {
+				return new Item(name, sellInDay, qualities);
+			});
+		});
+	});
+}
+
+function range(min: number, max: number) {
+	const length = max - min + 1;
+	return Array.from({ length }, (_, i: number) => min + i);
+}
