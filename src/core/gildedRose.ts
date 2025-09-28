31d2dca06e8e18733a386d74fc748354c2d9eafc
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-09-28 23:52:21 +0200

    refactor: introduce variable

--- src/core/gildedRose.ts
+++ src/core/gildedRose.ts
@@ -19,10 +19,12 @@ export class GildedRose {
 
 	updateQuality() {
 		for (let i = 0; i < this.items.length; i++) {
-			if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
+
+			const item = this.items[i];
+			if (item.name == 'Sulfuras, Hand of Ragnaros') {
 				continue;
 			}
-			if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+			if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
 				if (this.canDecreaseItemQuality(i)) {
 					this.decreaseItemQuality(i);
 				}
@@ -33,14 +35,14 @@ export class GildedRose {
 				}
 			}
 			this.decreaseItemSellIndays(i);
-			if (this.items[i].sellIn < 0) {
-				if (this.items[i].name != 'Aged Brie') {
-					if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+			if (item.sellIn < 0) {
+				if (item.name != 'Aged Brie') {
+					if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
 						if (this.canDecreaseItemQuality(i)) {
 							this.decreaseItemQuality(i);
 						}
 					} else {
-						this.items[i].quality = this.items[i].quality - this.items[i].quality;
+						item.quality = item.quality - item.quality;
 					}
 				} else {
 					if (this.canIncreaseQuality(i)) {
