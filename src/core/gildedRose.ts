c5a66a86caac0dff04499170818dc71be5fd8802
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-10-04 23:52:06 +0200

    Init Kata

--- src/core/gildedRose.ts
+++ src/core/gildedRose.ts
@@ -10,98 +10,59 @@ export class Item {
 	}
 }
 
-export class InnItem {
-	protected constructor(
-		protected name: string,
-		protected sellIn: number,
-		protected quality: number
-	) {}
-
-	static createFrom(item: Item) {
-		return InnItem.createFromNew(item);
-	}
-
-	static createFromNew(item: Item) {
-		switch (this.name) {
-			case 'Aged Brie':
-				return new AgedBrie('Aged Brie', item.sellIn, item.quality);
-			case 'Backstage passes to a TAFKAL80ETC concert':
-				return new BackstagePasses('Backstage passes to a TAFKAL80ETC concert', item.sellIn, item.quality);
-			case 'Sulfuras, Hand of Ragnaros':
-				return new Sulfuras('Sulfuras, Hand of Ragnaros', item.sellIn, item.quality);
-				break;
-			default:
-				return new InnItem(item.name, item.sellIn, item.quality);
-		}
-	}
-
-	updateQuality() {
-		if (this.quality > 0) {
-			this.quality = this.quality - 1;
-		}
-		this.sellIn = this.sellIn - 1;
-		if (this.sellIn < 0) {
-			if (this.quality > 0) {
-				this.quality = this.quality - 1;
-			}
-		}
-	}
-
-	toString() {
-		return `InnItem {
-    "name": "${this.name}",
-    "quality": ${this.quality},
-    "sellIn": ${this.sellIn},
-  }`;
-	}
-}
-
-class AgedBrie extends InnItem {
-	updateQuality() {
-		if (this.quality < 50) {
-			this.quality = this.quality + 1;
-		}
-		this.sellIn = this.sellIn - 1;
-		if (this.sellIn < 0) {
-			if (this.quality < 50) {
-				this.quality = this.quality + 1;
-			}
-		}
-	}
-}
-class BackstagePasses extends InnItem {
-	updateQuality() {
-		if (this.quality < 50) {
-			this.quality = this.quality + 1;
-			if (this.sellIn < 11) {
-				if (this.quality < 50) {
-					this.quality = this.quality + 1;
-				}
-			}
-			if (this.sellIn < 6) {
-				if (this.quality < 50) {
-					this.quality = this.quality + 1;
-				}
-			}
-		}
-		this.sellIn = this.sellIn - 1;
-		if (this.sellIn < 0) {
-			this.quality = this.quality - this.quality;
-		}
-	}
-}
-
-class Sulfuras extends InnItem {
-	updateQuality() {}
-}
-
 export class GildedRose {
-	constructor(public items: InnItem[]) {}
+	items: Array<Item>;
+
+	constructor(items = [] as Array<Item>) {
+		this.items = items;
+	}
 
 	updateQuality() {
-		this.items.forEach((item) => {
-			item.updateQuality();
-		});
+		for (let i = 0; i < this.items.length; i++) {
+			if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+				if (this.items[i].quality > 0) {
+					if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+						this.items[i].quality = this.items[i].quality - 1
+					}
+				}
+			} else {
+				if (this.items[i].quality < 50) {
+					this.items[i].quality = this.items[i].quality + 1
+					if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+						if (this.items[i].sellIn < 11) {
+							if (this.items[i].quality < 50) {
+								this.items[i].quality = this.items[i].quality + 1
+							}
+						}
+						if (this.items[i].sellIn < 6) {
+							if (this.items[i].quality < 50) {
+								this.items[i].quality = this.items[i].quality + 1
+							}
+						}
+					}
+				}
+			}
+			if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+				this.items[i].sellIn = this.items[i].sellIn - 1;
+			}
+			if (this.items[i].sellIn < 0) {
+				if (this.items[i].name != 'Aged Brie') {
+					if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+						if (this.items[i].quality > 0) {
+							if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+								this.items[i].quality = this.items[i].quality - 1
+							}
+						}
+					} else {
+						this.items[i].quality = this.items[i].quality - this.items[i].quality
+					}
+				} else {
+					if (this.items[i].quality < 50) {
+						this.items[i].quality = this.items[i].quality + 1
+					}
+				}
+			}
+		}
 
 		return this.items;
 	}
--- src/tests/gildedRose.test.ts
+++ src/tests/gildedRose.test.ts
@@ -1,32 +1,11 @@
-import { GildedRose, InnItem, Item } from '../core/gildedRose';
+import { Item, GildedRose } from '../core/gildedRose';
 
 describe('The Gilded Rose', () => {
 	it('updates quality for a new item', () => {
-		const gildedRose = new GildedRose(
-			generationCombinationOfItems(
-				['new item', 'Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Sulfuras, Hand of Ragnaros'],
-				range(-1, 11),
-				range(0, 51)
-			)
-		);
+		const gildedRose = new GildedRose([new Item('new item', 0, 0)]);
 
 		const items = gildedRose.updateQuality();
 
-		expect(items.map((item) => item.toString())).toMatchSnapshot();
+		expect(items[0]).toEqual({"name": "new item", "quality": 0, "sellIn": -1});
 	});
 });
-
-function generationCombinationOfItems(names: string[], sellInDays: number[], qualities: number[]): InnItem[] {
-	return names.flatMap((name) => {
-		return sellInDays.flatMap((sellInDay) => {
-			return qualities.flatMap((qualities) => {
-				return InnItem.createFrom(new Item(name, sellInDay, qualities));
-			});
-		});
-	});
-}
-
-function range(min: number, max: number) {
-	const length = max - min + 1;
-	return Array.from({ length }, (_, i: number) => min + i);
-}
