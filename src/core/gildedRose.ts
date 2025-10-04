89e516dd7719039327f5821a0efe8d080d1cd010
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-10-04 03:05:57 +0200

    red: apply polimorfism

--- src/core/gildedRose.ts
+++ src/core/gildedRose.ts
@@ -36,54 +36,6 @@ export class InnItem {
 	}
 
 	updateQuality() {
-		switch (this.name) {
-			case 'Aged Brie':
-				this.updateAgedBrieItemQuality();
-				break;
-			case 'Backstage passes to a TAFKAL80ETC concert':
-				this.updateBackstagePassItemQuality();
-				break;
-			case 'Sulfuras, Hand of Ragnaros':
-				break;
-			default:
-				this.updateStandardItemQuality();
-				break;
-		}
-	}
-
-	private updateAgedBrieItemQuality() {
-		if (this.quality < 50) {
-			this.quality = this.quality + 1;
-		}
-		this.sellIn = this.sellIn - 1;
-		if (this.sellIn < 0) {
-			if (this.quality < 50) {
-				this.quality = this.quality + 1;
-			}
-		}
-	}
-
-	private updateBackstagePassItemQuality() {
-		if (this.quality < 50) {
-			this.quality = this.quality + 1;
-			if (this.sellIn < 11) {
-				if (this.quality < 50) {
-					this.quality = this.quality + 1;
-				}
-			}
-			if (this.sellIn < 6) {
-				if (this.quality < 50) {
-					this.quality = this.quality + 1;
-				}
-			}
-		}
-		this.sellIn = this.sellIn - 1;
-		if (this.sellIn < 0) {
-			this.quality = this.quality - this.quality;
-		}
-	}
-
-	private updateStandardItemQuality() {
 		if (this.quality > 0) {
 			this.quality = this.quality - 1;
 		}
